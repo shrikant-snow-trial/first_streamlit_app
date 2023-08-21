91330b7a91e5fa9c62e2baa0a93c9da3ca492c2d
shrikant-snow-trial <142295776+shrikant-snow-trial@users.noreply.github.com>
2023-08-21 17:12:26 +0530

    Update streamlit_app.py
    deleted the simple json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,7 @@ streamlit.dataframe(fruits_to_show)
 #new text to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
-# Makes the json more readable
+# Take the json vserion of the response and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shows the json file in a table format
 streamlit.dataframe(fruityvice_normalized)
