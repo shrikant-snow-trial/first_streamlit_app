a0d06dc04bc7bb0a197eed96b691fab496fd6229
shrikant-snow-trial <142295776+shrikant-snow-trial@users.noreply.github.com>
2023-08-21 17:13:38 +0530

    Update streamlit_app.py
    appended the word kiwi in requests.get function

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 #new text to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # Take the json vserion of the response and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shows the json file in a table format
